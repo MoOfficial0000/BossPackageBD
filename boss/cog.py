e9fab48ce2f017b1477bf49af86cf120abf65c0b
MoOfficial0000 <muminhassouna123@gmail.com>
2024-12-15 07:49:37 +0000

    Bug fix for /boss conclude

--- boss/cog.py
+++ boss/cog.py
@@ -478,10 +478,10 @@ class Boss(commands.GroupCog):
         else:
             if len(totalnum) != 0:
                 bosswinner = totalnum[random.randint(0,len(totalnum)-1)][0]
-        await interaction.response.send_message(
-            f"Boss successfully concluded", ephemeral=True
-        )
         if bosswinner == 0:
+            await interaction.response.send_message(
+                f"Boss successfully concluded", ephemeral=True
+            )
             await interaction.channel.send(f"# Boss has concluded {self.bot.get_emoji(self.bossball.emoji_id)}\nThe boss has won the Boss Battle!")
             with open("totalstats.txt", "w") as file:
                 file.write(f"{total}{total2}")
@@ -502,7 +502,6 @@ class Boss(commands.GroupCog):
             self.disqualified = []
             return
         if winner != "None":
-            await interaction.response.defer(thinking=True)
             player, created = await Player.get_or_create(discord_id=bosswinner)
             special = special = [x for x in specials.values() if x.name == "Boss"][0]
             instance = await BallInstance.create(
@@ -512,6 +511,9 @@ class Boss(commands.GroupCog):
                 special=special,
                 attack_bonus=0,
                 health_bonus=0,
+            )
+            await interaction.response.send_message(
+                f"Boss successfully concluded", ephemeral=True
             )
             await interaction.channel.send(
                 f"# Boss has concluded {self.bot.get_emoji(self.bossball.emoji_id)}\n<@{bosswinner}> has won the Boss Battle!\n\n"
